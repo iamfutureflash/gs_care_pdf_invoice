e638d5f9ed3894cbef241f144e887caec25d1193
iamfutureflash <xenode2000@gmail.com>
2023-01-19 01:43:55 +0530

    updated app

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function App() {
   return (
-    <div className='bg-red-200 text-4xl text-center '>hi</div>
+    <div className='bg-red-200 text-4xl text-center '>hello puneet</div>
   )
 }
 
